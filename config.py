f68f012a108ed9ba953413f5f7da28e20c2d5b74
Daniel <danielhhowell@aol.com>
2018-05-04 00:49:15 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-class chromaTuneConfig:
+class config:
     def __init__(self):
         self.plotly_username = ''
         self.plotly_api_key = ''
@@ -8,10 +8,5 @@ class chromaTuneConfig:
         self.spotify_client_id = ''
         self.spotify_client_secret = ''
 
-    def plotly_credentials(self):
-        """Returns user and API key"""
-        return (self.plotly_username,self.plotly_api_key)
-
-    def spotify_credentials(self):
-        """Returns API client ID and client secret"""
-        return (self.spotify_client_id, self.spotify_client_secret)
+        self.spotify_username = ''
+        self.spotify_playlist = ''
